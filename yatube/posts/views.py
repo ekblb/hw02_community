1dbbed92c185d9f7cddbfbd1924511e99d94b81c
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-01-17 23:30:39 +0300

    Final project 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
-from django.conf import settings
 
 
 def index(request):
