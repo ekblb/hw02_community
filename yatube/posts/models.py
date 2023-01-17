a00011248940883e34109a8efa8552f6f67b6b4c
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-01-17 23:08:17 +0300

    Fix bugs after review 2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -19,13 +19,15 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts')
+        related_name='posts'
+    )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
         related_name='posts',
-        on_delete=models.SET_NULL,)
+        on_delete=models.SET_NULL,
+    )
 
     class Meta:
         ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
-from yatube.settings import NUMBER_OF_POSTS
+from django.conf import settings
 
 
 def index(request):
-    posts = Post.objects.select_related()[:NUMBER_OF_POSTS]
+    posts = Post.objects.select_related(
+        'author', 'group')[:settings.NUMBER_OF_POSTS]
     context = {
         'posts': posts,
     }
@@ -14,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:NUMBER_OF_POSTS]
+    posts = group.posts.all()[:settings.NUMBER_OF_POSTS]
     context = {
         'group': group,
         'posts': posts,
